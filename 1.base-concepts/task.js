78497e0fd03d19a5c1d1edddd87922d7fd6128de
Gioza <karagiozov.a@mail.ru>
2021-09-08 16:30:30 +0300

    Task done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,56 @@
+
+"use strict"; 
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let D = b ** 2 - (4 * a * c);
+  if(D < 0) {
+    return [];
+  }
+ 
+  if(D == 0) {
+    let x = -b / (2 * a);
+    return [x];
+  }
+  
+  if (D > 0) {
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    return[x1, x2];
+  }
+  
+  }
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
+    if((typeof percent) == 'string')
+        percent = Number(percent);
+    if((typeof contribution) == 'string')
+        contribution = Number(contribution);
+    if((typeof amount) == 'string')
+        amount = Number(amount);
 
-  return totalAmount;
+    if( percent < 0 )
+        return `Параметр percent содержит неправильное значение ${percent}.`
+    if(contribution <0)
+         return `Параметр contribution содержит неправильное значение ${contribution}.`
+    if( amount<0)
+          return `Параметр amount содержит неправильное значение ${amount}.`
+    if( date<new Date())
+          return `Параметр date содержит неправильное значение ${date}.`
+
+    let S = amount - contribution;
+    let today = new Date();
+    let yearDiff = date.getFullYear() - new Date.getFullYear();
+    let monthDiff = date.getMonth() - new Date.getMonth();
+    let months = (year_diff * 12) + month_diff;
+    let P = (percent / 12) / 100;
+    let monthAmount = S * (P + P / (((1 + P) ** months) - 1));
+    let totalAmount = months * monthAmount;
+
+    console.log(totalAmount.toFixed(2));
+    
+    return Number(totalAmount.toFixed(2));
 }
