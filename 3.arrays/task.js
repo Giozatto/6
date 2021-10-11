cb2023eef52e6573a8177667f6436bfe8fa58160
Gioza <karagiozov.a@mail.ru>
2021-10-11 22:23:45 +0300

    Task done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,43 +1,20 @@
-// Задача 1
+//Задача 1
 
-// function compareArrays(arr1, arr2) {
+function compareArrays(arr1, arr2) {
 
   
-//   let result = (arr1.length == arr2.length) && arr1.every((item, index) =>
-//     item === arr2[index]); 
-//   return result; 
-// }
-// compareArrays([8, 9], [8, 9])
-
-
-// // Задача 2
-
-// function advancedFilter(arr) {
-//   let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
-
-//   return resultArr; 
-// }
-
-function Myevery(arr1,func)
-{
-  let res=false;
-  for(let i=0;i<arr1.length; i++){
-    res=func(arr1[i],i,arr1);
-    if(res==false)
-    {
-      return false;
-    }
-  }
-  return true;
+  let result = (arr1.length == arr2.length) && arr1.every((item, index) =>
+    item === arr2[index]); 
+  return result; 
 }
-function cmp(item,index,arr)
-{
-if(item==arr2[index])
-return false;
-else
- return true;
+compareArrays([8, 9], [8, 9])
+
+
+// Задача 2
+
+function advancedFilter(arr) {
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
+
+  return resultArr; 
 }
-let a1=[1,2,3,4];
-let arr2=[1,2,3,4];
-let res=Myevery(a1,cmp);
-console.log(res);
+
